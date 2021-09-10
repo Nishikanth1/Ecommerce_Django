5b944ba8e91fa58d24f5e7aab2aa8ac422dbcaed
Nishikanth1 <35811911+Nishikanth1@users.noreply.github.com>
2021-09-10 21:33:32 +0530

    Products  (#1)
    * products app init
    
    * product views and templates
    
    * some updates
    
    * some changes
    
    * better
    
    * products_almost_done
    
    * Update .gitignore

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', views.all_products, name='all_products'),
+    path('item/<int:id>/', views.product_detail, name='product_detail'),
+    path('item/create/', views.product_create, name='product_create'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list')
+]
